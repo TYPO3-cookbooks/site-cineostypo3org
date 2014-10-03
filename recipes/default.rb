68bd6793452e5a4abc328743157ca02f7e2579f8
Jacob Floyd <cognifloyd@gmail.com>
2014-10-03 15:28:59 -0400

    [TASK] Switch to Jenkins LTS

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 include_recipe "jenkins::master"
+# This makes it use Jenkins LTS:
+r = resources("apt_repository[jenkins]")
+r.uri 'http://pkg.jenkins-ci.org/debian-stable'
+r.key 'http://pkg.jenkins-ci.org/debian-stable/jenkins-ci.org.key'
