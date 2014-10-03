38d2a98ba15aec076f19091bb175a72b08f08469
Jacob Floyd <cognifloyd@gmail.com>
2014-10-03 13:35:50 -0400

    [TASK] berks cookbook site-cineostypo3org .

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'site-cineostypo3org'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures site-cineostypo3org'
+long_description 'Installs/Configures site-cineostypo3org'
+version          '0.1.0'
+
