04bfd958e13105ffa940d91fc6c4721b93596ec1
Jacob Floyd <cognifloyd@gmail.com>
2014-10-04 12:21:07 -0400

    [BUGFIX] favicon filename

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['site-cineostypo3org']['hostname'] = 'ci-neos.typo3.org'
 default['site-cineostypo3org']['ssl_certificate'] = 'wildcard.typo3.org'
-default['site-cineostypo3org']['favicon'] = 'typo3_and_jenkins.png'
+default['site-cineostypo3org']['favicon'] = 'typo3-and-jenkins.png'
 
 # plugin installation didn't suceed within 60 or 180 seconds, so make it longer
 default['jenkins']['executor']['timeout'] = 360
